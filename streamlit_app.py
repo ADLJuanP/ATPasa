0f4d47517695ce1aea368a7687c89cec61524b8a
ADLJuanP <adljpaillaguala@gmail.com>
2024-12-19 10:16:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,40 +94,43 @@ else:
 
             # Configuración de la figura
             fig, ax1 = plt.subplots(figsize=(14, 8))
-
+            
+            # Agregar título
+            ax1.set_title("Gráfico de ATPasa por Mes-Día y Categoría Externa", fontsize=16)
+            
             # Crear la paleta de colores
             palette = sns.color_palette("pastel", n_colors=filtered_df['C. Externa'].nunique()).as_hex()
-
+            
             # Crear el boxplot usando 'Mes-Dia'
             sns.boxplot(x=filtered_df['Mes-Dia'], y=filtered_df['ATPasa'], showfliers=False, color="lightblue", ax=ax1)
-
+            
             # Crear el stripplot
             sns.stripplot(x=filtered_df['Mes-Dia'], y=filtered_df['ATPasa'], hue=filtered_df['C. Externa'],
                           jitter=True, alpha=0.7, palette=palette, dodge=True, ax=ax1, legend=False)
-
+            
             # Ordenar las etiquetas en el eje X de acuerdo a la columna 'Fecha'
             ax1.set_xticks(range(len(ordered_labels)))
             ax1.set_xticklabels(ordered_labels, rotation=90)
-
+            
             # Quitar el título del eje x
             ax1.set_xlabel(None)
             ax1.set_ylabel("ATPasa", fontsize=12)
-
+            
             # Crear segundo eje (para gráfico de barras apiladas)
             ax2 = ax1.twinx()
-
+            
             percentages = filtered_df.groupby(['Mes-Dia', 'C. Externa']).size().unstack(fill_value=0)
             percentages = percentages.div(percentages.sum(axis=1), axis=0) * 100
-
+            
             # Ordenar el gráfico de barras según 'Mes-Dia' (usando el mismo orden que ax1)
             percentages = percentages.loc[ordered_labels]
-
+            
             percentages.plot(kind='bar', stacked=True, ax=ax2, alpha=0.3, width=0.5, color=palette)
-
+            
             ax2.set_ylabel("% de Categoría", fontsize=12)
             ax2.set_ylim(0, 100)
             ax2.grid(visible=False)
             ax2.legend(title="Condición Externa", bbox_to_anchor=(1.15, 0.5), loc='center')
-
+            
             # Mostrar el gráfico en Streamlit
             st.pyplot(fig)
